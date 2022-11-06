b5d604dce9f07ea30ca02b6336479b090b3d5209
zhaoyi <766470375@qq.com>
2022-11-06 14:03:00 +0800

    设置端口和网站标题

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Project_hrsaas',
+  title: '人力资源管理平台',
 
   /**
    * @type {boolean} true | false
--- vue.config.js
+++ vue.config.js
@@ -6,23 +6,23 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
+const name = defaultSettings.title || '人力资源管理平台' // 网页标题
 
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
+// 如果端口设置为80，
+// 使用管理员权限执行命令行。
+// 例如, Mac: sudo npm run
+// 您可以通过以下方法修改端口:
 // port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
+//所有的配置项说明可以在https://cli.vuejs.org/config/中找到
 module.exports = {
   /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
+   * 如果您计划将站点部署到子路径下，例如GitHub Pages，则需要设置publicPath。
+   * 如果您计划将站点部署到https://foo.github.io/bar/，
+   * 那么应该将publicPath设置为“/bar/”。
+   * 在大多数情况下请使用'/' !!
+   * 细节:https://cli.vuejs.org/config/ 公共路径
    */
   publicPath: '/',
   outputDir: 'dist',
@@ -38,8 +38,8 @@ module.exports = {
     }
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
+    // 在webpack的name字段中提供应用程序的标题，这样
+    // 它可以在index.html中访问，以注入正确的标题。
     name: name,
     resolve: {
       alias: {
@@ -48,21 +48,21 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
+    // 它可以提高第一屏的速度，建议打开预载
     config.plugin('preload').tap(() => [
       {
         rel: 'preload',
-        // to ignore runtime.js
+        // 忽略runtime.js
         // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
         fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
         include: 'initial'
       }
     ])
 
-    // when there are many pages, it will cause too many meaningless requests
+    // 当页面很多的时候，它会引起太多无意义的请求
     config.plugins.delete('prefetch')
 
-    // set svg-sprite-loader
+    // 设置svg-sprite-loader
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
@@ -98,22 +98,23 @@ module.exports = {
                   name: 'chunk-libs',
                   test: /[\\/]node_modules[\\/]/,
                   priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
+                  chunks: 'initial' //只打包最初依赖的第三方
                 },
                 elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                  name: 'chunk-elementUI', // 将elementUI拆分为单个包
+                  priority: 20, // 权重需要大于libs和app，否则它将被打包到libs或app中
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // 为了适应cnpm
                 },
                 commons: {
                   name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
+                  test: resolve('src/components'), // 可以自定义规则
+                  minChunks: 3, //   最低限度共同氧化数
                   priority: 5,
                   reuseExistingChunk: true
                 }
               }
             })
+
           // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
           config.optimization.runtimeChunk('single')
         }
