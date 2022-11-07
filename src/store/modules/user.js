8cff7596a8342555dad8bf7cb5b0f059d81fa804
zhaoyi <766470375@qq.com>
2022-11-07 17:34:44 +0800

    完成请求拦截器及获取用户信息

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,4 @@
-import {login} from "@/api/user";
+import {login, getUserInfo} from "@/api/user";
 import {getToken, setToken, removeToken} from "@/utils/auth";
 
 export default {
@@ -6,7 +6,10 @@ export default {
   namespaced: true,
 
   state: {
-    token: getToken()
+    // 存储token
+    token: getToken(),
+    // 存储用户信息
+    userInfo: {}
   },
   mutations: {
     // 设置token
@@ -19,12 +22,27 @@ export default {
     removeToken(state) {
       state.token = null // 删除vuex的token
       removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+    },
+    // 设置用户信息
+    setUserInfo(state, userInfo) {
+      state.userInfo = { ...userInfo } // 用 浅拷贝的方式去赋值对象 因为这样数据更新之后，才会触发组件的更新
+    },
+    // 删除用户信息
+    removeUserInfo(state) {
+      state.userInfo = {}
     }
   },
   actions: {
+    // 登录
     async login(context, data) {
       const result = await login(data)
       context.commit('setToken', result)
+    },
+    // 获取用户资料action
+    async getUserInfo(context) {
+      const result = await getUserInfo()  // 获取返回值
+      context.commit('setUserInfo', result) // 将整个的个人信息设置到用户的vuex数据中
+      return result // 这里为什么要返回 为后面埋下伏笔
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,16 @@ const service = axios.create({
 })// 创建axios实例
 
 // 请求拦截器
-service.interceptors.request.use()
+service.interceptors.request.use(config => {
+  // 在这个位置需要统一的去注入token
+  if (store.getters.token) {
+    // 如果token存在 注入token
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config // 必须返回配置
+}, error => {
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 service.interceptors.response.use(response => {
