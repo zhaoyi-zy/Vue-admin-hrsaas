94343dbcb1457ae367e9958ff463545abebd9ef9
zhaoyi <766470375@qq.com>
2022-11-13 19:05:29 +0800

    静态路由与动态路由临时合并，形成左侧菜单

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,16 @@ Vue.use(Router)
 
 import Layout from '@/layout'
 
+// 引入多个模块的规则
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
+
 /**
  * 常用路由
  * 没有权限要求的基本页面
@@ -31,18 +41,31 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: {title: '首页', icon: 'dashboard'}
     }]
   },
 
   // 404页必须放在末尾!!
-  { path: '*', redirect: '/404', hidden: true }
+  {path: '*', redirect: '/404', hidden: true}
+]
+
+// 动态路由
+export const asyncRoutes = [
+  approvalsRouter,
+  attendancesRouter,
+  salarysRouter,
+  socialRouter,
+  settingRouter,
+  employeesRouter,
+  departmentsRouter,
+  permissionRouter
+
 ]
 
 const createRouter = () => new Router({
   //模式:'history'， //需要服务支持
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  scrollBehavior: () => ({ y: 0 }), // 管理滚动行为 如果出现滚动 切换就让 让页面回到顶部
+  routes: [...constantRoutes, ...asyncRoutes] // 临时合并所有的路由
 })
 
 const router = createRouter()
